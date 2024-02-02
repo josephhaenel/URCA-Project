01a3fc836e67177bd2060904cb4deda375ac1851
Joe <josephhaenel@gmail.com>
2024-02-01 20:45:51 -0600

    Refactoring

--- DLrun.py
+++ DLrun.py
@@ -20,33 +20,46 @@ def run_all_models(base_directories):
         
         # Base output directory
         last_part = os.path.basename(base_dir)
-        if not os.path.exists('outputs', 'outputs' + last_part):
-            os.makedirs('outputs', 'outputs' + last_part)
-        base_output_dir = 'outputs' + last_part
+        number = 1
+        while True:
+            if not os.path.exists(os.path.join('outputs', str(number), 'outputs' + last_part)):
+                os.makedirs(os.path.join('outputs', str(number), 'outputs' + last_part))
+                break
+            number += 1    
+        base_output_dir = os.path.join('outputs', str(number), 'outputs' + last_part)
         
+        learning_rate = 0.0001 # 0.001, 0.0001
+        val_split = 0.7 # 0.9, 0.7, 0.5, 0.3, 0.1
+        
+        infoPath = os.path.join('outputs', str(number), 'info.txt')
+            
+        with open(infoPath, "w") as f:
+            # Write learning rate and validation split to info.txt file
+            f.write("learningRate = " + str(learning_rate) + "\n" + "valSplit = " + str(val_split))
+            
         
         # Run ResNet50 Model
         resnet50_output_dir = os.path.join(base_output_dir, 'ResNet50')
-        resnet50_model = ResNet50Model(base_rgb_dir, base_disease_dir, base_leaf_dir)
+        resnet50_model = ResNet50Model(base_rgb_dir, base_disease_dir, base_leaf_dir, learning_rate, val_split)
         resnet50_history = resnet50_model.compile_and_train(epochs=10, batch_size=32, output_dir=resnet50_output_dir)
         
                 # Run AlexNet Model
         alexnet_output_dir = os.path.join(base_output_dir, 'AlexNet')
-        alexnet_model = AlexNetModel(base_rgb_dir, base_disease_dir, base_leaf_dir)
+        alexnet_model = AlexNetModel(base_rgb_dir, base_disease_dir, base_leaf_dir, learning_rate, val_split)
         alexnet_history = alexnet_model.compile_and_train(epochs=10, batch_size=32, output_dir=alexnet_output_dir)
         
             # Run Inception ResNet V2 Model
         inception_resnet_v2_output_dir = os.path.join(base_output_dir, 'InceptionResNetV2')
-        inception_resnet_v2_model = InceptionResNetV2Model(base_rgb_dir, base_disease_dir, base_leaf_dir)
+        inception_resnet_v2_model = InceptionResNetV2Model(base_rgb_dir, base_disease_dir, base_leaf_dir, learning_rate, val_split)
         inception_resnet_v2_history = inception_resnet_v2_model.compile_and_train(epochs=10, batch_size=32, output_dir=inception_resnet_v2_output_dir)
     
     
-    send_email(
-        "Model Training Complete",
-        "The training of all models has been completed successfully.",
-        "jhaenel@siue.edu",
-        "josephhaenel@gmail.com"
-    )
+    # send_email(
+    #     "Model Training Complete",
+    #     "The training of all models has been completed successfully.",
+    #     "jhaenel@siue.edu",
+    #     "josephhaenel@gmail.com"
+    # )
     
 def send_email(subject, message, recipient_email, sender_email):
     msg = MIMEMultipart()
